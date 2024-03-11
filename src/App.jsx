75e28d3e36ff71740c4bb04e6004ca832588ad5a
ablakesmith <a.blake.smith11@gmail.com>
2024-03-11 12:02:40 -0400

    Browser Parking Lot repo setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,60 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from 'react';
+import { nanoid } from "nanoid";
+import ParkingLotForm from "./Components/ParkingLotForm/ParkingLotForm";
+import ParkingLotList from "./Components/ParkingLotList/ParkingLotList";
+
+function getInitialState() {
+    let savedState = localStorage.getItem('items');
+    if (typeof savedState === 'string') {
+        return JSON.parse(savedState);
+    }
+    return [];
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [parkingLotItems, setParkingLotItems] = useState(getInitialState());
+
+    function saveParkingLotItems() {
+        localStorage.setItem('items', JSON.stringify(parkingLotItems));
+    }
+
+    useEffect(saveParkingLotItems, [ parkingLotItems ]);
+
+    function addItem(date, link, description, priority) {
+        setParkingLotItems((oldItems) => [
+                ...oldItems, 
+                {
+                    id: nanoid(),
+                    date,
+                    link,
+                    description,
+                    priority,
+                }
+        ]);
+    }
+
+    function deleteItem(id) {
+        setParkingLotItems(
+            (oldItems) => oldItems.filter(item => item.id !== id)
+        ); 
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Browser Parking Lot</h1>
+                <p>Send most of your browser tabs into retirement.</p>
+            </header>
+            <main>
+                <ParkingLotForm addItem={addItem} />
+                <ParkingLotList 
+                    parkingLotItems={parkingLotItems} 
+                    deleteItem={deleteItem} />
+            </main>
+        </div>
+    );
 }
 
 export default App;
